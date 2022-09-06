96b0974f7d92a51c724a6cdfd92ff24fc72f6a0d
Andrew Morton <andrewmorton2@hotmail.co.uk>
2022-09-06 16:57:00 +0100

    Caret now blinks!

--- js/terminal.js
+++ js/terminal.js
@@ -6,7 +6,7 @@ var headingString =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 const parentDiv = document.getElementById("main");
-const caretDiv = document.getElementById("caret");
+const caretNode = document.getElementById("caret");
 
 function loadLineText() {}
 
@@ -21,7 +21,7 @@ function beginLine() {
 function drawLine(text) {
   const pNode = document.createElement("p");
   //pNode.id = "mainp";
-  parentDiv.insertBefore(pNode, caretDiv);
+  parentDiv.insertBefore(pNode, caretNode);
 
   const interval = setInterval(function () {
     //textNode.textContent += text.charAt(0);
@@ -31,11 +31,28 @@ function drawLine(text) {
     text = text.substring(1);
     if (text == "") {
       clearInterval(interval);
-      parentDiv.insertBefore(document.createElement("br"), caretDiv);
-      parentDiv.insertBefore(document.createElement("br"), caretDiv);
+      parentDiv.insertBefore(document.createElement("br"), caretNode);
+      parentDiv.insertBefore(document.createElement("br"), caretNode);
     }
   }, 20);
 }
+
+// setInterval to blink caret cursor.
+const backgroundColor = window.getComputedStyle(document.body).backgroundColor; // I love javascript <3
+console.log(backgroundColor);
+
+const caretColor = getComputedStyle(caretNode).backgroundColor;
+
+setInterval(function() {
+  // If it's filled, then blank it (make it the background colour).
+  // Otherwise fill it again.
+  // It blinks!
+  if (getComputedStyle(caretNode).backgroundColor == caretColor) {
+    caretNode.style.backgroundColor = backgroundColor;
+  } else {
+    caretNode.style.backgroundColor = caretColor;
+  }
+}, 530);
 
 drawLine(headingString);
 //drawText("vvvvvvvvvvvvvvvvv");

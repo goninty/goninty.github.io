dfe5908d26dcd480337d34ef61ad53fc26de9890
Andrew Morton <andrewmorton2@hotmail.co.uk>
2022-09-08 16:14:59 +0100

    Typing working (mostly). Needing final tweaks.

--- js/terminal.js
+++ js/terminal.js
@@ -1,87 +1,105 @@
 // You know what? I'll fix this later.
 const pageText = `## ABOUT ##
 
-> I'm Andrew.
-> A recent Computer Science graduate from the University of Bath. 
-> Looking for graduate software development positions.
-> I like building fun things and playing rhythm games.
+I'm Andrew.
+A recent Computer Science graduate from the University of Bath. 
+Looking for graduate software development positions.
+I like building fun things and playing rhythm games.
 
 
 ## TECHNICAL SKILLS ##
 
-> C#, C/C++, Java
-> Python, JS
-> HTML/CSS
-> React/Bootstrap
-> VCS (Git, GitHub, BitBucket)
+C#, C/C++, Java
+Python, JS
+HTML/CSS
+React/Bootstrap
+VCS (Git, GitHub, BitBucket)
 
 
 ## EXPERIENCE ##
 
-> PAL Leader (Peer Assisted Learning)
-> Senior PAL Leader
-> Academic Representative
+PAL Leader (Peer Assisted Learning)
+Senior PAL Leader
+Academic Representative
 
 
 ## PROJECTS ##
 
-> MiiCloner~https://github.com/goninty/MiiCloner
-> University Simulator~https://devpost.com/software/university-simulator-nultrm
-> Autistica Data Visualisations~https://github.com/Lon19/team-7
-> AskReddit eBooks~https://github.com/goninty/askreddit_ebooks
+MiiCloner~https://github.com/goninty/MiiCloner
+University Simulator~https://devpost.com/software/university-simulator-nultrm
+Autistica Data Visualisations~https://github.com/Lon19/team-7
+AskReddit eBooks~https://github.com/goninty/askreddit_ebooks
 
-> GitHub~https://github.com/goninty
+GitHub~https://github.com/goninty
 
 
 ## CONTACT ##
 
-> LinkedIn~https://www.linkedin.com/in/andrew-morton-325b45187
-> Discord: Ninty#7513`;
+LinkedIn~https://www.linkedin.com/in/andrew-morton-325b45187
+Discord: Ninty#7513`;
 
 var pageLines = pageText.split("\n");
+var finished = false;
 
 const parentDiv = document.getElementById("main");
 const caretNode = document.getElementById("caret");
 
 // I wish there were a better way to do this where the string stayed const.
 // requestAnimationFrame/setTimeout/setInterval do not play nice when wrapped inside a loop, though.
-function drawLine(text, i) {
-   var pNode;
+function drawLine(text, i, noBreak) {
+  var node;
 
   // If link, create link. Otherwise regular paragraph tag.
   if (text.includes('~')) {
-    pNode = document.createElement('a');
+    node = document.createElement('a');
     [text, link] = text.split('~');
-    pNode.href = link;
+    node.href = link;
   } else {
-    pNode = document.createElement('p');
+    node = document.createElement('p');
   }
 
-  parentDiv.insertBefore(pNode, caretNode);
+  parentDiv.insertBefore(node, caretNode);
 
   const interval = setInterval(function () {
-    pNode.innerHTML += text.charAt(0);
+    node.innerHTML += text.charAt(0);
     text = text.substring(1);
 
     // Once line has finished, stop interval and go next line.
     if (text == "") {
       clearInterval(interval);
-      parentDiv.insertBefore(document.createElement("br"), caretNode);
+
+      if (!noBreak) {
+        parentDiv.insertBefore(document.createElement("br"), caretNode);
+      }
       //parentDiv.insertBefore(document.createElement("br"), caretNode);
 
       // Recurse to begin drawing a new line.
       // Has to be recursive so that it is called after the current line has been drawn.
       // Otherwise setInterval pushes the function calls whenever it can,
       // so lines are drawn in random orders instead of sequentially.
-      if (i > 0) drawLine(pageLines[pageLines.length - i], i - 1);
+      if (i > 0) {
+        // This is awful.
+        if (noBreak || pageLines[pageLines.length - i] == '' || pageLines[pageLines.length - i].charAt(0) == '#') {
+          drawLine(pageLines[pageLines.length - i], i - 1, false);
+        } else {
+          drawLine("> ", i, true);
+        }
+      } else {
+        // We are finished writing the text.
+        // Please try branchless programming.
+        if (!finished) {
+          finished = true;
+          parentDiv.insertBefore(document.createElement("br"), caretNode);
+          const event = new Event('finishedWriting');
+          document.dispatchEvent(event);
+        }
+      }
     }
-  }, 40);
+  }, 10);
 }
 
 // setInterval to blink caret cursor.
-const backgroundColor = window.getComputedStyle(document.body).backgroundColor; // I love javascript <3
-console.log(backgroundColor);
-
+const backgroundColor = window.getComputedStyle(document.body).backgroundColor;
 const caretColor = getComputedStyle(caretNode).backgroundColor;
 
 setInterval(function() {
@@ -95,4 +113,33 @@ setInterval(function() {
   }
 }, 530);
 
-drawLine(pageLines[0], pageLines.length-1);
+document.addEventListener('finishedWriting', function() {
+  parentDiv.insertBefore(document.createElement('p'), caretNode);
+  document.addEventListener("keydown", function (e) {
+    switch (e.key) {
+      case "Enter":
+        //drawLine("", 0, false);
+        parentDiv.insertBefore(document.createElement("br"), caretNode);
+        parentDiv.insertBefore(document.createElement("p"), caretNode);
+        break;
+      case "Backspace":
+        var typedText = caretNode.previousSibling.textContent;
+        console.log(typedText);
+        if (typedText.length == 0) {
+          caretNode.previousSibling.remove();
+          if (caretNode.previousSibling.nodeName == "BR") caretNode.previousSibling.remove();
+        } else {
+          caretNode.previousSibling.innerHTML = typedText.slice(0, -1);
+        }
+        break;
+      default:
+        if (e.key.length == 1) {
+        //drawLine(e.key, 0, true);
+          caretNode.previousSibling.innerHTML += e.key;
+        }
+        break;
+    }
+  });
+});
+
+drawLine(pageLines[0], pageLines.length-1, false);
